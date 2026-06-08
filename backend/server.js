5ed7b1f2bb3d24d670a38d5fa329be76951ea213
Rithu-4 <rithurajan2000@gmail.com>
2026-06-08 12:48:18 +0530

    changes made to project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
@@ -9,7 +9,7 @@ const orderRoutes = require("./routes/orderRoutes");
 
 const authRoutes = require("./routes/authRoutes");
 
-dotenv.config();
+ require("dotenv").config();
 
 const app = express();
 
